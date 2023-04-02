4876896fbe987e703b135315c5881ccde2ed9c3d
Victor Inojosa <vijoin@gmail.com>
2023-04-02 13:05:35 -0300

    Inital commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,14 @@
+// Raffle
+
+// Enter the lottery (paying some amount)
+// Pick a random number (verifiable random)
+// winner to be selected every X minutes -> completely automated
+// chainlink oracle -> randomness, automated execution (chainlink keeper)
+
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.18;
+
+contract Raffle {}
+
